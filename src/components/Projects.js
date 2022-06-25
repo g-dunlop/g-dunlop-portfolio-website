37421e723b0c748d6cb1fc4b272985388cc2df81
g-dunlop <graemedunlop1983@gmail.com>
2022-06-25 15:19:01 +0100

    added tractorFactor to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ import gHub from "../static/icons/ghub.png";
 import MemoryGameVid from "../static/memory/MemoryGameVid.mp4";
 import vmaVid from "../static/vma/vmaVid.mp4";
 import stockTrackerVid from "../static/stockTracker/StockTracker2Vid.mp4";
-
+import TractorFactor from "../static/tractorfactor/TractorFactor.mp4"
 
 
 const Projects = () => { 
@@ -49,7 +49,7 @@ const Projects = () => {
         color:white;
         font-size:1.5rem;
         text-align:left;
-        margin-top:3rem;
+        margin-top:2rem;
         margin-bottom:0.5rem;
         letter-spacing:1px;
         @media (max-width:900px){
@@ -101,14 +101,29 @@ const Projects = () => {
         <div id="projects">
             <Title>Projects</Title>
             <Row />
+            <ProjectSection className="project" id="tf">
+                
+                <Div className="description">
+                    <ProjectHeader>TractorFactor</ProjectHeader>
+                    <Paragraph><span className="bolding">About: </span>Full-Stack app built on the last 2 weeks of the CodeClan course.  This was a group project that took 12 days.</Paragraph>
+                    <Paragraph><span className="bolding">Purpose: </span>The app allows an online tractor auction site to quickly find a qualified person to authenticate the sold vehicle.</Paragraph>
+                    <Paragraph><span className="bolding">Tools: </span> Built with React, React Bootstrap, Java, Google Maps API, Spring Boot, h2 SQL database.</Paragraph>
+                    <Paragraph><GitDiv><span className="bolding">ReadMe: </span><a className="project-icon" href="https://github.com/g-dunlop/Tractor_Inspector_Finder_App" target="_blank"><Git className="project-icon" src={gHub}/></a></GitDiv></Paragraph> 
+                </Div>
+                <video className="project-vid" width="400" height="300" autoPlay muted controls loop>
+                    <source src={TractorFactor} type="video/mp4"></source>
+                </video>
+            </ProjectSection>
+           
+            <Row />
             <ProjectSection className="project" id="st">
-                {/* <a href=""><Pic src={StockTracker_1}></Pic></a> */}
                 <video className="project-vid" width="400" height="300" autoPlay muted controls loop>
                     <source src={stockTrackerVid} type="video/mp4"></source>
                 </video>
                 <Div className="description">
                     <ProjectHeader>Stock Tracker</ProjectHeader>
                     <Paragraph><span className="bolding">About: </span>Full-Stack app built on week 10 of the CodeClan course.  This was a group project that took 6 days.</Paragraph>
+                    <Paragraph><span className="bolding">Purpose: </span>The app allows a small-scale trader to track and analyse the stocks they hold in their portfolio.</Paragraph>
                     <Paragraph><span className="bolding">Tools: </span> Built with React, Node js, Express, MongoDB, APIs, Git.</Paragraph>
                     <Paragraph><GitDiv><span className="bolding">ReadMe: </span><a className="project-icon" href="https://github.com/g-dunlop/stock_tracker_app_v2" target="_blank"><Git className="project-icon" src={gHub}/></a></GitDiv></Paragraph>
                     
@@ -120,6 +135,7 @@ const Projects = () => {
                 <Div className="description">
                     <ProjectHeader>Memory Game</ProjectHeader>
                     <Paragraph><span className="bolding">About: </span>Full-stack app built on week 8 of the CodeClan course.  This was an individual project that took 3 days.</Paragraph>
+                    <Paragraph><span className="bolding">Purpose: </span>This was an extra project I set myself to see if I could replicate the memory game 'Simon', which I played as a young child.</Paragraph>
                     <Paragraph><span className="bolding">Tools: </span>Built with React, Node js, Express, MongoDB.</Paragraph>
                     <Paragraph><GitDiv><span className="bolding">ReadMe: </span><a className="project-icon" href="https://github.com/g-dunlop/memory-game" target="_blank"><Git className="project-icon" src={gHub}/></a></GitDiv></Paragraph>
                     
@@ -138,6 +154,7 @@ const Projects = () => {
                 <Div className="description">
                     <ProjectHeader>Vet Management App</ProjectHeader>
                     <Paragraph><span className="bolding">About: </span>Full-Stack app built on week 5 of the CodeClan course.  This was an individual project that took 6 days.</Paragraph>
+                    <Paragraph><span className="bolding">Purpose: </span>The app allows veterinary practice to keep records of their customers, their pets, and the bookings made at the practice.</Paragraph>
                     <Paragraph><span className="bolding">Tools: </span>Built with Python, Flask, Jinja, PostgreSQL.</Paragraph>
                     <Paragraph><GitDiv><span className="bolding">ReadMe: </span><a className="project-icon" href="https://github.com/g-dunlop/Project-1-Vet-Management-App" target="_blank"><Git className="project-icon" src={gHub}/></a></GitDiv></Paragraph>
                     
