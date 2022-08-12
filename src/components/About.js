52ccd05adfe9ba6aacc432eb700087620d0454d0
g-dunlop <graemedunlop1983@gmail.com>
2022-08-12 11:34:48 +0100

    slight amendment to about section

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ const About = () => {
                 <img className="headshot" src={headshot} alt="headshot"></img>
                 <div className="about-container">
                     <p className="about-paragraph">Hi! I’m a software developer with a background in language education. I have strong organisational, communication and interpersonal skills built over the last 15 years of working as a teacher, mentor, manager and trainer in various countries around the world. </p>
-                    <p className="about-paragraph">I’m a pro-active problem-solver looking for a junior role which provides opportunities for growth and development during the early stages of my development career.</p>
+                    <p className="about-paragraph">I’m a pro-active problem-solver looking for a role which provides opportunities for growth and development.</p>
                 </div>
             </div>
         </div>
